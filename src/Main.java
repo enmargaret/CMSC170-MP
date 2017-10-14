8255b22e18f54014741644366c96ca7426624775
Ena <enamarga@gmail.com>
2017-10-14 13:23:35 +0800

    First commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Ena on 14/10/2017.
+ */
+import java.io.*;
+
+public class Main {
+    public static void main (String [] args) {
+        System.out.println("Creating Repository...");
+
+    }
+
+}
